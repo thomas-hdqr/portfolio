5bd52691696e05e5b7c0539f1b034dfe6932da0c
Thomas <thomashaudiquer@gmail.com>
2022-08-03 16:46:12 +0100

    add some padding

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -1,12 +1,11 @@
 export default function About() {
     return (
         <div id="about" className="about">
-                <div>
-                    <p>Thomas Haudiquer<br></br>
-                    I am a staff designer based in New York, by way of Berlin and originally from New Zealand. Currently at Linktree where I help define the future of the web.</p>
+                <div className="about-text-1">
+                    <p>Thomas Haudiquer</p>
+                   <p>I am a staff designer based in New York, by way of Berlin and originally from New Zealand. Currently at Linktree where I help define the future of the web.</p>
                 </div>
-                    <div>
-                        <p>A passion for Web </p>
+                    <div className="about-text-2">
                         <p>With a background in design, I work closely with design focused teams to build websites and microsites for companies and individuals. I have years of experience working and collaborating on product teams and leading engineering efforts.</p>
                     </div>
         </div>
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,8 @@
 export default function Contact() {
 
     return (
-            <div id="contact" className="contact">
-                <h2>Want to work together?</h2>
+            <div className="contact">
+                <h2 className="contact-work">Want to work together?</h2>
                 <a className="emailaddress" href="mailto:thomashaudiquer@gmail.com">thomashaudiquer@<br></br>gmail.com</a>
                 <div className="footer-menu">
                     <p><a href="#">linkedin</a></p>
