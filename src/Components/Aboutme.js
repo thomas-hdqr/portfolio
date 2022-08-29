93de446a403782e6b78e814cbfb515f65476526f
Thomas <thomashaudiquer@gmail.com>
2022-08-29 14:37:40 +0100

    adding my picture

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -1,13 +1,20 @@
+
+
 export default function About() {
     return (
-        <section id="about" className="about">
+        <section id="about">
+            <div className="about">
                 <div className="about-text-1">
                     <p>Thomas Haudiquer</p>
-                   <p>I am a staff designer based in New York, by way of Berlin and originally from New Zealand. Currently at Linktree where I help define the future of the web.</p>
+                    <p>I am a staff designer based in New York, by way of Berlin and originally from New Zealand. Currently at Linktree where I help define the future of the web.</p>
                 </div>
-                    <div className="about-text-2">
-                        <p>With a background in design, I work closely with design focused teams to build websites and microsites for companies and individuals. I have years of experience working and collaborating on product teams and leading engineering efforts.</p>
-                    </div>
+                <div className="about-text-2">
+                    <p>With a background in design, I work closely with design focused teams to build websites and microsites for companies and individuals. I have years of experience working and collaborating on product teams and leading engineering efforts.</p>
+                </div>
+            </div>
+            <div className="me-image">
+               <img src="./images/polaroid-low-resolution.png" alt="" />
+            </div>
         </section>
     )
 }
